06820700acc511d84ff19a71c7dcbf8d93a976a1
inassiperboi <inas.najah.zhahirah-2022@vokasi.unair.ac.id>
2026-09-22 17:19:19 +0700

    update 22 sept

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -94,9 +94,9 @@
             </div>
 
             <div class="flex items-center gap-4">
-                <a href="tel:03189451234" class="hover:underline flex items-center gap-1">
+                <a href="tel:+6281233020117" class="hover:underline flex items-center gap-1">
                     <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                    (031) 8945-1234
+                    +62 8123-3020117
                 </a>
 
                 <span class="text-slate-300">•</span>
